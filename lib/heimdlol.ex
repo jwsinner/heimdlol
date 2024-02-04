bcea6cbee4e4c3f76aa21e0fe9443e16d5e57cdc
jwsinner <jwsinner@gmail.com>
2024-02-04 12:49:56 -0700

    initial commit

---
+++ lib/heimdlol.ex
@@ -0,0 +1,18 @@
+defmodule Heimdlol do
+  @moduledoc """
+  Documentation for `Heimdlol`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Heimdlol.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/heimdlol_test.exs
@@ -0,0 +1,8 @@
+defmodule HeimdlolTest do
+  use ExUnit.Case
+  doctest Heimdlol
+
+  test "greets the world" do
+    assert Heimdlol.hello() == :world
+  end
+end
